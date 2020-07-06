215ec76e1578f679593918ffc6ae1b3c6e818f3f
Rihab JOUINI <rihab.jouini@mobile-devices.fr>
2020-07-06 15:50:22 +0200

    Updating crates to display the temperature result

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@
 #[allow(unused_extern_crates)] 
 #[allow(unused_imports)]
 
-//use serde::{Serialize, Deserialize}; // imports both the trait and the derive macro
-use panic_halt;
+extern crate panic_semihosting ;
+
+//use panic_halt;
 
 //use core::result::Result;
 //use heapless::{consts, Vec}; 
@@ -15,6 +16,11 @@ use stm32f4xx_hal as hal;
 use stm32f4xx_hal::adc::Adc;
 use hal::adc::Temperature;
 
+use cortex_m_semihosting::hprintln; 
+//use hal::prelude::*; //for split et constrain and other exti
+
+
+
 
 #[rt::entry]
 fn main() -> ! {
@@ -23,15 +29,25 @@ fn main() -> ! {
        let mut temp_adc = Adc::adc1(p.ADC1,true,config_adc); //constr
        temp_adc.enable_temperature_and_vref(); // enable 
        temp_adc.start_conversion();
-       
+       //let gpiob=p.GPIOB.split();
+       /*let mut led1 = gpiob.pb0.into_push_pull_output();
+       let mut led2 = gpiob.pb7.into_push_pull_output();*/
+      
        
      loop{
          
-        let mut adc_sample = temp_adc.convert(&Temperature,stm32f4xx_hal::adc::config::SampleTime::Cycles_480);
-        let mut millivolts = temp_adc.sample_to_millivolts(adc_sample);
-        let mut temp = ((millivolts - 760)as f32  / 2.5 ) + 25.0 ; 
-        println!("Temperature {}", temp);
-     
+       let  adc_sample = temp_adc.convert(&Temperature,stm32f4xx_hal::adc::config::SampleTime::Cycles_480);
+       let  millivolts = temp_adc.sample_to_millivolts(adc_sample);
+       let  temp = ((millivolts - 760)as f32  / 2.5 ) + 25.0 ; 
+       //led1.set_high().unwrap();
+       /*if temp > 10.0 {
+        led1.set_low().unwrap();
+       }
+       if temp <10.0 {
+           led2.set_high().unwrap();
+       }*/
+         hprintln!("The temperature is : {} ",temp).unwrap(); 
+
        
  
 
